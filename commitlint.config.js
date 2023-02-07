f136ef40844d43bcdfc7d800b404c1bd3fc73c23
Yvan Nanfack <romyvan1999@gmail.com>
2023-02-07 04:58:23 +0100

    chore(settings): setup commitlint for commit message validation and environment files

---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+// Conventionnal commit principles: https://www.conventionalcommits.org/
+// more: https://blog.tericcabrel.com/apply-conventional-commit-style-on-your-project-with-commitlint/
+
+const conventionalCommit = require("./conventionalCommit.json");
+
+const typesEnum = Object.keys(conventionalCommit.types);
+const scopesEnum = Object.keys(conventionalCommit.scopes);
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", typesEnum],
+    "scope-case": [2, "always", ["camel-case"]],
+    "scope-enum": [2, "always", scopesEnum],
+    "subject-empty": [2, "never"],
+    "subject-case": [2, "always", ["lower-case"]],
+    "header-max-length": [2, "always", 100],
+  },
+};
