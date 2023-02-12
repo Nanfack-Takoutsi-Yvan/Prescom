06387fb0b24a729e68b4253d225791370acbf5c5
Yvan Nanfack <contact@yvannanfack.com>
2023-02-12 03:33:35 +0100

    feat(app): setup unit test with jest

---
+++ loadershim.js
@@ -0,0 +1,7 @@
+/**
+ * This file is require for jest to work
+ * Learn more: https://www.gatsbyjs.com/docs/how-to/testing/unit-testing/#2-creating-a-configuration-file-for-jest
+ */
+global.___loader = {
+  enqueue: jest.fn(),
+}
