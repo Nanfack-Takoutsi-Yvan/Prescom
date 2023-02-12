80c73dd34bf5d684bf5bf322f10af9c92a2f32ca
Yvan Nanfack <contact@yvannanfack.com>
2023-02-12 06:41:35 +0100

    chore(app): setup mui, use custom font and icons

--- src/components/ui/title.test.tsx
+++ src/components/ui/title.test.tsx
@@ -3,12 +3,14 @@ import { render } from '@testing-library/react'
 import renderer from 'react-test-renderer'
 
 // You have to write data-testid
-const Title = () => <h1 data-testid="hero-title">Gatsby is awesome!</h1>
+const Title = () => <h1 data-testid="hero-title">Gatsby is realy awesome!</h1>
 
 test('Displays the correct title', () => {
   const { getByTestId } = render(<Title />)
   // Assertion
-  expect(getByTestId('hero-title')).toHaveTextContent('Gatsby is awesome!')
+  expect(getByTestId('hero-title')).toHaveTextContent(
+    'Gatsby is realy awesome!'
+  )
   // --> Test will pass
 })
 
