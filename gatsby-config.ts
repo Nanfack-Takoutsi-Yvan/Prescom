f3496907144883091d99eff8ccae676123afb31b
Yvan Nanfack <contact@yvannanfack.com>
2023-11-04 21:10:55 +0100

    chore(app): add page layout

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,13 +14,16 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
+  plugins: [
+  {
     resolve: 'gatsby-source-contentful',
     options: {
       accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       spaceId: process.env.CONTENTFUL_SPACE_ID,
     },
-  }, 'gatsby-plugin-image', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', 'gatsby-plugin-google-gtag', 'gatsby-plugin-sass', 'gatsby-plugin-react-helmet', {
+  },
+    'gatsby-plugin-layout', 'gatsby-plugin-image', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', 'gatsby-plugin-google-gtag', 'gatsby-plugin-sass',
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       name: 'images',
---
+++ src/layouts/index.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/jsx-no-undef */
+import * as React from 'react'
+import { type PageProps } from 'gatsby'
+import useCursor from '../services/hooks/cursor'
+import Navbar from '../components/ui/navbar/navbar'
+import { useLocalization } from '../services/hooks/localization'
+import { AppStateContext } from '../services/contexts/appStateContext'
+import './index.module.scss'
+
+const MainLayout: React.FC<PageProps> = ({ children }) => {
+  const [locale, setLocale] = React.useState<string>('')
+
+  useCursor()
+  const { content, locales } = useLocalization(locale, setLocale)
+
+  const value = React.useMemo<AppContextProps>(
+    () => ({
+      locale,
+      locales,
+      content,
+      setLocale
+    }),
+    []
+  )
+
+  return (
+    <AppStateContext.Provider value={value}>
+      <main>
+        <Navbar />
+        {children}
+      </main>
+    </AppStateContext.Provider>
+  )
+}
+
+export default MainLayout
