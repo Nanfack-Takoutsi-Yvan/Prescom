aa75c1344667ac0704ed642d37e3d74fdb6ec147
Yvan Nanfack <contact@yvannanfack.com>
2023-11-04 21:34:32 +0100

    fix(app): add firebase configs

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@
 import type { GatsbyConfig } from 'gatsby'
 
 require("dotenv").config({ // eslint-disable-line
-  path: `.env.${process.env.NODE_ENV}`,
+  path: `.env`,
 })
 
 const config: GatsbyConfig = {
@@ -15,61 +15,64 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-  {
-    resolve: 'gatsby-source-contentful',
-    options: {
-      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      spaceId: process.env.CONTENTFUL_SPACE_ID,
-    },
-  },
-    'gatsby-plugin-layout', 'gatsby-plugin-image', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', 'gatsby-plugin-google-gtag', 'gatsby-plugin-sass',
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      name: 'images',
-      path: './src/images/',
-    },
-    __key: 'images',
-  },
-  {
-    resolve: `gatsby-plugin-google-gtag`, // learn more: https://www.gatsbyjs.com/plugins/gatsby-plugin-google-gtag/
-    options: {
-      trackingIds: [
-        process.env.GA_TRACKING_ID,
-      ],
-      pluginConfig: {
-        head: true,
-        anonynize: true,
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
       },
     },
-  },
-  {
-    resolve: `gatsby-omni-font-loader`,
-    options: {
-      enableListener: true,
-      preconnect: [`https://www.cdnfonts.com/`, `https://www.cdnfonts.com/euclid-circular-b.font`],
-      web: [
-        {
-          name: `Euclid Circular B`,
-          file: `https://fonts.cdnfonts.com/css/euclid-circular-b`,
+      'gatsby-plugin-layout', 'gatsby-plugin-image', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', 'gatsby-plugin-google-gtag', 'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://www.cdnfonts.com/`, `https://www.cdnfonts.com/euclid-circular-b.font`],
+        web: [
+          {
+            name: `Euclid Circular B`,
+            file: `https://fonts.cdnfonts.com/css/euclid-circular-b`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
         },
-      ],
-    },
-  },
-  {
-    resolve: 'gatsby-plugin-react-svg',
-    options: {
-      rule: {
-        include: /assets/,
       },
     },
-  },
-  {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/prescom.png"
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/prescom.png"
+      }
+    },
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSENGER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+          measurementId: process.env.FIREBASE_MEASUREMENT_ID
+        }
+      }
     }
-  }],
+  ],
 }
 
 export default config
